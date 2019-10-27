10e3ddca1ac831a5e54d3116e373e4da180ec186
bo-jia <wooacat@gmail.com>
2019-10-27 21:42:25 +0800

    refine coco evaluation output pose by selecting best candidate

--- joints_detectors/Alphapose/train_sppe/src/evaluation.py
+++ joints_detectors/Alphapose/train_sppe/src/evaluation.py
@@ -62,6 +62,84 @@ def add_candidate_joints(result, hms, pt1, pt2, inpH, inpW, oupH, oupW):
         res['can_kps'] = can_kps
         res['can_sco'] = can_sco
 
+def select_best_candidate(gt_json, final_result):
+    mirror_map = {
+            1:2, 2:1, 3:4, 4:3, 5:6, 6:5, 7:8, 8:7, 9:10, 10:9, 11:12, 12:11, 13:14, 14:13, 15:16, 16:15
+        }
+    
+    with open(gt_json, 'r') as f:
+        gt = json.load(f)['annotations']
+    gt_map = {}
+    for g in gt:
+        if g['image_id'] in gt_map:
+            gt_map[g['image_id']].append(g['keypoints'])
+        else:
+            gt_map[g['image_id']] = [g['keypoints']]
+    
+    def compute_pose_dist(pose, pose_gt):
+        n = len(pose_gt) // 3
+        d = 0
+        v = 0
+        for i in range(n):
+            if pose_gt[i*3+2] > 0:
+                d += np.linalg.norm(np.array(pose[i*2:i*2+2]) - np.array(pose_gt[i*3:i*3+2]))
+                v += 1
+        return d / v
+
+    def match_pose(pose, gt_list):
+        if len(gt_list) == 1:
+            return gt_list[0]
+        else:
+            min_idx = 0
+            min_dst = np.finfo(np.float32).max
+            for i, gt in enumerate(gt_list):
+                d = compute_pose_dist(pose, gt)
+                if d < min_dst:
+                    min_dst = d
+                    min_idx = i
+            return gt_list[min_idx]
+
+    def get_min_joint(joint, can_joints):
+        min_d = np.finfo(np.float32).max
+        min_j = None
+        for j in can_joints:
+            d = np.linalg.norm(joint, j)
+            if d < min_d:
+                min_d = d
+                min_j = j
+        return min_j
+
+    def select_best_candidate(kps, can_kps, gt_kps):
+        n = len(gt_kps) // 3
+        assert n == kps.shape[0]
+        for i in range(n):
+            if gt_kps[i*3+2] > 0:                
+                if i in mirror_map:
+                    if i < mirror_map[i]:
+                        can_joints = [kps[i], kps[mirror_map[i]]]
+                        if can_kps[i] is not None: can_joints.append(can_kps[i])
+                        if can_kps[mirror_map[i]] is not None: can_joints.append(can_kps[mirror_map[i]])
+                        lj = get_min_joint(np.array(pose_gt[i*3:i*3+2]), can_kps)
+                        rj = get_min_joint(np.array(pose_gt[mirror_map[i]*3:mirror_map[i]*3+2]), can_kps)
+                        if np.linalg.norm(lj, rj) > 2:
+                            kps[i] = lj
+                            kps[mirror_map[i]] = rj
+                else:
+                    if can_kps[i] is not None:
+                        min_d = np.linalg.norm(kps[i] - np.array(pose_gt[i*3:i*3+2]))
+                        can_d = np.linalg.norm(can_kps[i].numpy() - np.array(pose_gt[i*3:i*3+2]))
+                        if can_d < min_d:
+                            kps[i] = can_kps[i].numpy()
+        return kps
+
+    for result in final_result:
+        image_id = int(result['imgname'].split('/')[-1].split('.')[0].split('_')[-1])
+        for pose in result['result']:
+            kps = pose['keypoints'].numpy()
+            best_match_pose = match_pose(kps.reshape(-1), gt_map[image_id])
+            refine_pose = select_best_candidate(kps, pose['can_kps'], best_match_pose)
+            pose['keypoints'] = torch.from_numpy(refine_pose)
+    
 
 def prediction(model, img_folder, boxh5, imglist):
     if torch.cuda.is_available():
@@ -150,7 +228,8 @@ def prediction(model, img_folder, boxh5, imglist):
         #cv2.imwrite(os.path.join(
         #    './val', 'vis', im_name), img)
         final_result.append(result)
-
+        
+    select_best_candidate('../../examples/coco_val/person_keypoints_val2017.json', final_result)
     write_json(final_result, '../../examples/coco_val', for_eval=True)
     return getmap()
 
