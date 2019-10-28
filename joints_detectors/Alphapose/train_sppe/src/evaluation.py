556e0534a0001c1f1b5a5214cbeaea3760e98ccc
Yin Zhang <yzhang@cs.utexas.edu>
2019-10-28 16:30:30 +0800

    edit

--- joints_detectors/Alphapose/train_sppe/src/evaluation.py
+++ joints_detectors/Alphapose/train_sppe/src/evaluation.py
@@ -18,6 +18,7 @@ import os
 import cv2
 from models.FastPose import createModel
 import argparse
+import json
 
 def gaussian(size):
     '''
@@ -40,18 +41,18 @@ def bbox_iou(box1, box2):
     
     """
     #Get the coordinates of bounding boxes
-    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
-    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[0], box1[1], box1[2], box1[3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[0], box2[1], box2[2], box2[3]
     
     #get the corrdinates of the intersection rectangle
-    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
-    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
-    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
-    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    inter_rect_x1 =  max(b1_x1, b2_x1)
+    inter_rect_y1 =  max(b1_y1, b2_y1)
+    inter_rect_x2 =  min(b1_x2, b2_x2)
+    inter_rect_y2 =  min(b1_y2, b2_y2)
     
     #Intersection area
     
-    inter_area = torch.max(inter_rect_x2 - inter_rect_x1 + 1,torch.zeros(inter_rect_x2.shape).cuda())*torch.max(inter_rect_y2 - inter_rect_y1 + 1, torch.zeros(inter_rect_x2.shape).cuda())
+    inter_area = max(inter_rect_x2 - inter_rect_x1 + 1,0)*max(inter_rect_y2 - inter_rect_y1 + 1, 0)
     #Union Area
     b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
     b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
@@ -110,7 +111,7 @@ def select_best_candidate(gt_json, final_result):
             if pose_gt[i*3+2] > 0:
                 d += np.linalg.norm(np.array(pose[i*2:i*2+2]) - np.array(pose_gt[i*3:i*3+2]))
                 v += 1
-        return d / v
+        return d / v if v > 0 else np.finfo(np.float32).max
 
     def match_pose(pose, gt_list):
         if len(gt_list) == 1:
@@ -129,7 +130,7 @@ def select_best_candidate(gt_json, final_result):
         min_d = np.finfo(np.float32).max
         min_j = None
         for j in can_joints:
-            d = np.linalg.norm(joint, j)
+            d = np.linalg.norm(joint-j)
             if d < min_d:
                 min_d = d
                 min_j = j
@@ -143,28 +144,37 @@ def select_best_candidate(gt_json, final_result):
                 if i in mirror_map:
                     if i < mirror_map[i]:
                         can_joints = [kps[i], kps[mirror_map[i]]]
-                        if can_kps[i] is not None: can_joints.append(can_kps[i])
-                        if can_kps[mirror_map[i]] is not None: can_joints.append(can_kps[mirror_map[i]])
-                        lj = get_min_joint(np.array(pose_gt[i*3:i*3+2]), can_kps)
-                        rj = get_min_joint(np.array(pose_gt[mirror_map[i]*3:mirror_map[i]*3+2]), can_kps)
-                        if np.linalg.norm(lj, rj) > 2:
+                        if can_kps[i] is not None: 
+                            can_joints.append(can_kps[i].numpy())
+                        if can_kps[mirror_map[i]] is not None: 
+                            can_joints.append(can_kps[mirror_map[i]].numpy())
+                        lj = get_min_joint(np.array(gt_kps[i*3:i*3+2]), can_joints)
+                        rj = get_min_joint(np.array(gt_kps[mirror_map[i]*3:mirror_map[i]*3+2]), can_joints)
+                        if np.linalg.norm(lj-rj) > 2:
                             kps[i] = lj
                             kps[mirror_map[i]] = rj
                 else:
                     if can_kps[i] is not None:
-                        min_d = np.linalg.norm(kps[i] - np.array(pose_gt[i*3:i*3+2]))
-                        can_d = np.linalg.norm(can_kps[i].numpy() - np.array(pose_gt[i*3:i*3+2]))
+                        min_d = np.linalg.norm(kps[i] - np.array(gt_kps[i*3:i*3+2]))
+                        can_d = np.linalg.norm(can_kps[i].numpy() - np.array(gt_kps[i*3:i*3+2]))
                         if can_d < min_d:
                             kps[i] = can_kps[i].numpy()
         return kps
 
-    def check_pose(pose0, pose1, threshold=0.5):
+    def check_pose(pose0, pose_gt, threshold=0.5):
+        pose1 = np.array(pose_gt).reshape(-1,3)
+        pose_idx = (pose1[:,2] > 0)
+        if not np.any(pose_idx):
+            return False
+        pose0 = pose0[pose_idx]
+        pose1 = pose1[:,:2][pose_idx]
+
         box0_lt = np.min(pose0, axis=0)
         box0_rb = np.max(pose0, axis=0)
         
         box1_lt = np.min(pose1, axis=0)
         box1_rb = np.max(pose1, axis=0)
-
+        
         box0 = np.array([box0_lt[0], box0_lt[1], box0_rb[0], box0_rb[1]])
         box1 = np.array([box1_lt[0], box1_lt[1], box1_rb[0], box1_rb[1]])
 
@@ -174,10 +184,11 @@ def select_best_candidate(gt_json, final_result):
 
     for result in final_result:
         image_id = int(result['imgname'].split('/')[-1].split('.')[0].split('_')[-1])
+        if image_id not in gt_map: continue
         for pose in result['result']:
             kps = pose['keypoints'].numpy()
             best_match_pose = match_pose(kps.reshape(-1), gt_map[image_id])
-            if check_pose(kps, np.array(best_match_pose).reshape(-1,3)[:,2]):
+            if check_pose(kps, best_match_pose):
                 refine_pose = select_best_candidate(kps, pose['can_kps'], best_match_pose)
                 pose['keypoints'] = torch.from_numpy(refine_pose)
             else:
@@ -257,9 +268,9 @@ def prediction(model, img_folder, boxh5, imglist):
             )
 
             result = pose_nms(box, preds_img, preds_scores)
-
-            add_candidate_joints(result, kp_preds.cpu().numpy(), pt1.numpy(), pt2.numpy(), opt.inputResH, opt.inputResW, opt.outputResH, opt.outputResW)
-
+            
+            # add_candidate_joints(result, kp_preds.cpu().numpy(), pt1.numpy(), pt2.numpy(), opt.inputResH, opt.inputResW, opt.outputResH, opt.outputResW)
+            
             result = {
                 'imgname': im_name,
                 'result': result
@@ -271,8 +282,8 @@ def prediction(model, img_folder, boxh5, imglist):
         #cv2.imwrite(os.path.join(
         #    './val', 'vis', im_name), img)
         final_result.append(result)
-        
-    select_best_candidate('../../examples/coco_val/person_keypoints_val2017.json', final_result)
+    # np.savez('../../examples/coco_val/final_result.npz', result=result)
+    # select_best_candidate('../../examples/coco_val/person_keypoints_val2017.json', final_result)
     write_json(final_result, '../../examples/coco_val', for_eval=True)
     return getmap()
 
